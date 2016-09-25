e93bf760aace6c2497cf85b1e686d44752e3181c
CHEN TZE CHENG <imchenchen1994@hotmail.com>
2016-09-25 14:26:55 +0800

    added slippery, non fiction and fiction tiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public Texture dirBg;
     public Texture dirNeedle;
     public int needleSpeed;
+	private bool ifFriction;
+	private bool ifSlippery;
+
 
     private Rigidbody2D rb;
     private int angle;
@@ -19,6 +22,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         winText.text = "";
         angle = 0;
+		ifFriction = false;
+		ifSlippery = false;
     }
 
     void OnGUI()
@@ -33,13 +38,25 @@ public class PlayerController : MonoBehaviour
     {
         angle = (angle + needleSpeed) % 360;
 
+		if (ifSlippery == true) {
+			rb.drag = 0.01f;
+		}
+		else if (ifFriction == true) {
+
+			rb.drag = 3;
+
+		} else if (ifSlippery == false && ifFriction == false){
+			rb.drag = 1f;
+		}
+
+
         if (Input.GetKeyDown("space"))
         {
             float angleRad = angle * Mathf.Deg2Rad;
             float moveHorizontal = Mathf.Sin(angleRad);
             float moveVertical = Mathf.Cos(angleRad);
             Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-            rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed );
         }
 
         if (sizeSlider.value == 0)
@@ -47,20 +64,27 @@ public class PlayerController : MonoBehaviour
             winText.text = "Level Cleared!";
         }
 
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Pickup"))
-        {
-            other.gameObject.SetActive(false);
-            sizeSlider.value += 10;
-        }
-        else if (other.gameObject.CompareTag("Getthin"))
-        {
-            other.gameObject.SetActive(false);
-            sizeSlider.value -= 10;
-        }
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			sizeSlider.value += 10;
+		} else if (other.gameObject.CompareTag ("Getthin")) {
+			other.gameObject.SetActive (false);
+			sizeSlider.value -= 10;
+		} else if (other.gameObject.CompareTag ("Friction")) {
+			ifFriction = true;
+			ifSlippery = false;
+		} else if (other.gameObject.CompareTag ("NonFriction")) {
+			ifFriction = false;
+			ifSlippery = false;
+		} else if (other.gameObject.CompareTag ("Slippery")) {
+			ifSlippery = true;
+		}
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -69,7 +93,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Wall"))
         {
             sizeSlider.value -= 10;
-        }
+		}
     }
+		
 
 }
